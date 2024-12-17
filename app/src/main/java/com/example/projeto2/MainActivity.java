a6575283ce7de3284cd00dba722a7d2b25e60ddd
joaoneto458 <joaoacacio628@gmail.com>
2024-12-17 11:28:46 -0300

    commit inicial

---
+++ app/src/main/java/com/example/projeto2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.projeto2;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // Assuming the layout file is activity_main.xml
+    }
+}
